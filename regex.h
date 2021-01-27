9a27631d26676403d55f88126729baa899b3ca29
Ben Payne <trixtur@gmail.com>
2021-01-26 22:40:42 -0700

    Started documenting regex.h

--- regex.h
+++ regex.h
@@ -1,7 +1,28 @@
+/**
+ *
+ */
 int match(char *regexp, char *text);
+/**
+ *
+ */
 int matchhere(char *regexp, char *text);
+/**
+ *
+ */
 int matchstar(int c, char *regexp, char *text);
+/**
+ *
+ */
 int matchstar_d(char *regexp, char *text);
+/**
+ *
+ */
 int checkdigit(char* text);
+/**
+ *
+ */
 int matchstar_w(char *regexp, char *text);
+/**
+ * checkword - check if a word exists at the current character.
+ */
 int checkword(char* text);
